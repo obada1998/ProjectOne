14def1465fc51f78b45fb5a4cf1f5be2ae7cadeb
obada jazaerli <obada.ja8@gmail.com>
2024-04-04 02:44:45 +0300

    The bug has been fixed

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 Console.WriteLine("Hello, World!");
 // Where is the message that should appear to the user??
+Console.WriteLine("Enter the message ,Please :");
 print(Console.ReadLine()!);
 static void print(string message) =>
     Console.WriteLine(message);
