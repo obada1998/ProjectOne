1b1afebe4bfe18e4db9d54a50c99b2851534f272
obada1998 <108441899+obada1998@users.noreply.github.com>
2024-04-04 02:41:10 +0300

    Fix bug!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 Console.WriteLine("Hello, World!");
-
+// Where is the message that should appear to the user??
 print(Console.ReadLine()!);
 static void print(string message) =>
     Console.WriteLine(message);
